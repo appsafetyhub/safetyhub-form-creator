48700fd264e69341800eef009013d07db9004d91
Matheus Machado <matheus.machado@beaverbit.com.br>
2023-01-16 13:56:14 -0300

    commit v0.0.4

--- src/toolbar-draggable-item.jsx
+++ src/toolbar-draggable-item.jsx
@@ -1,11 +1,11 @@
 /**
-  * <ToolbarItem />
-  */
+ * <ToolbarItem />
+ */
 
-import React from 'react';
-import { DragSource } from 'react-dnd';
-import ItemTypes from './ItemTypes';
-import ID from './UUID';
+import React from "react";
+import { DragSource } from "react-dnd";
+import ItemTypes from "./ItemTypes";
+import ID from "./UUID";
 
 const cardSource = {
   beginDrag(props) {
@@ -22,14 +22,18 @@ class ToolbarItem extends React.Component {
   render() {
     const { connectDragSource, data, onClick } = this.props;
     if (!connectDragSource) return null;
-    return (
-      connectDragSource(
-        <li className='py-4 shadow d-flex flex-column align-items-center justify-content-center gap-2' onClick={onClick}><i className={data.icon}></i>{data.name}</li>,
-      )
+    return connectDragSource(
+      <li
+        className="py-4 shadow d-flex flex-column align-items-center text-center justify-content-center gap-2"
+        onClick={onClick}
+      >
+        <i className={data.icon}></i>
+        {data.name}
+      </li>
     );
   }
 }
 
-export default DragSource(ItemTypes.CARD, cardSource, connect => ({
+export default DragSource(ItemTypes.CARD, cardSource, (connect) => ({
   connectDragSource: connect.dragSource(),
 }))(ToolbarItem);
